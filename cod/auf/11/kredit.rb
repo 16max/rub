ac0cabd8020951c3e089f87678b2121e62db170c
16max <schelemax@g16hh.de>
2015-10-15 11:53:02 +0200

    15.10.2015

--- cod/auf/11/kredit.rb
+++ cod/auf/11/kredit.rb
@@ -1,13 +1,13 @@
 class Kredit
 	attr_accessor :kredit
-	attr_writer :jahresgehalt
+	attr_writer :jahresgehalt 
 
 	def initialize(kundendaten)
 		@kunde = kundendaten
 	end
-	def kundendaten
-		"#{@kunde} Jahresgehalt: #{@jahresgehalt} Euro Kredit: #{@kredit} Euro"
-	end
+	 def kundendaten
+	 	"#{@kunde} Jahresgehalt: #{@jahresgehalt} € Kredit: #{@kredit} €"
+	 end
 	def kreditvergabe
 		if @jahresgehalt >= 6 * @kredit
 			"Der Kredit in Höhe von #{@kredit} kann gewährt werden"
