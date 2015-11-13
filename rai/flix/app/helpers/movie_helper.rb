83a4c123fc1c2c97e3373336b8192ccc337303ac
16max <schelemax@g16hh.de>
2015-11-13 12:23:34 +0100

    13.11.2015

--- rai/flix/app/helpers/movie_helper.rb
+++ rai/flix/app/helpers/movie_helper.rb
@@ -1,2 +1,21 @@
 module MovieHelper
+		def ein_mehrzahl
+			if @movies.size == 1
+				"Movie"
+			else
+				"Movies"
+			end
+		end
+
+		def top_or_flop(movie)
+			if movie.total_gross < 50000000
+				"Flop!"
+			else
+				number_to_currency(movie.total_gross, unit: "€ ")
+			end
+		end
+
+		def text_verkürzt(movie)
+			truncate(movie.description, length: 40, seperator: "")
+		end
 end
