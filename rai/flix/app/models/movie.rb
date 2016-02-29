76235688ff6c56e117e43167b2d9dde56ab0d280
16max <schelemax@g16hh.de>
2016-02-29 09:21:56 +0100

    29.02.2016

--- rai/flix/app/models/movie.rb
+++ rai/flix/app/models/movie.rb
@@ -1,12 +1,12 @@
 class Movie < ActiveRecord::Base
 	has_many :reviews
 
-	validates_presence_of :titel, :release, :duration, :message => "muss ausgefüllt sein"
-	validates_length_of :description, :minimum => 25, allow_blank: true, :message => "ist zu kurz. Die Mindestlänge beträgt 25 Zeichen."
-	validates_numericality_of :total_gross, numericality: { only_integer: true }, :greater_than => 0, :message => "ist Null oder keine Zahl."
+	#validates_presence_of :titel, :release, :duration, :message => "muss ausgefüllt sein"
+	#validates_length_of :description, :minimum => 25, allow_blank: true, :message => "ist zu kurz. Die Mindestlänge beträgt 25 Zeichen."
+	#validates_numericality_of :total_gross, numericality: { only_integer: true }, :greater_than => 0, :message => "ist Null oder keine Zahl."
 	#validates_format_of :poster_image_file, :with => /gif\z/
-	validates_format_of :poster_image_file, :with => %r{\.(gif\z|jpg\z|png\z)}, :message => "darf nur im gif-, jpg- oder png-Format sein."
-	validates_inclusion_of :rating, in: %w( G PG PG-13 R NC-17 ), :message => "benötigt die Angabe: G, PG, PG-13, R oder NC-17"
+	#validates_format_of :poster_image_file, :with => %r{\.(gif\z|jpg\z|png\z)}, :message => "darf nur im gif-, jpg- oder png-Format sein."
+	#validates_inclusion_of :rating, in: %w( G PG PG-13 R NC-17 ), :message => "benötigt die Angabe: G, PG, PG-13, R oder NC-17"
 
 	def rating=(val)
 		self[:rating] = val.upcase
